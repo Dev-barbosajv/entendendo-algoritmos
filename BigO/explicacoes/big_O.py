4ba01207e4cdfb52544666300f2752ade541e802
Dev-barbosajv <joaovitorbarbosa513@gmail.com>
2025-03-26 21:53:48 -0300

    Regra de ouro

---
+++ BigO/explicacoes/big_O.py
@@ -0,0 +1,7 @@
+"""
+🧠 Regra de ouro:
+Busca binária só funciona com listas ordenadas!
+
+Se a lista não estiver em ordem, ele pode ignorar elementos onde a resposta está — e vai te dar resultado errado ou None.
+"""
+
